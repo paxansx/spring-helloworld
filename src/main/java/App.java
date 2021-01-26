99eff4b93a0d47145445129898b5854527a84ce7
paxansx <60510220+paxansx@users.noreply.github.com>
2021-01-26 19:07:13 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        boolean check = bean==bean1;
+
+        System.out.println(bean==bean1);
+
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+        System.out.println(cat==cat1);
+
+
     }
 }
